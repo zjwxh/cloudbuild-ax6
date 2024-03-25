43a3549ac519e832e463280a63cd755357a1722b
zjwxh <61620117+zjwxh@users.noreply.github.com>
2024-03-25 13:58:59 +0800

    Update diy-part2.sh

--- custom/redmi_ax6000/hanwckf/diy-part2.sh
+++ custom/redmi_ax6000/hanwckf/diy-part2.sh
@@ -13,9 +13,13 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 ##-----------------Add alist-----------------
-#sudo apt update
-#sudo apt install libfuse-dev
+sudo apt update
+sudo apt install libfuse-dev
 #rm -rf feeds/packages/lang/golang
 #svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
 #git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 ##---------------------------------------------------------
