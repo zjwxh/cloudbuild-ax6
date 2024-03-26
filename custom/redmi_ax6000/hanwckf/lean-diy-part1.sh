3c05338e04ee7415353398f7f23eb8692c49a618
zjwxh <61620117+zjwxh@users.noreply.github.com>
2024-03-26 14:32:04 +0800

    Update lean-diy-part1.sh

--- custom/redmi_ax6000/hanwckf/lean-diy-part1.sh
+++ custom/redmi_ax6000/hanwckf/lean-diy-part1.sh
@@ -20,4 +20,4 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 #echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 #sed -i '$a src-git kenzok8 https://github.com/kenzok8/small-package' feeds.conf.default
 #sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
