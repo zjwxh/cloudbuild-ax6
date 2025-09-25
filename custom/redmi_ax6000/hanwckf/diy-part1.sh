3cdf5b9002ebaaf851430813afe5f43138aaf1b9
zjwxh <61620117+zjwxh@users.noreply.github.com>
2025-09-25 15:26:50 +0800

    Update diy-part1.sh

--- custom/redmi_ax6000/hanwckf/diy-part1.sh
+++ custom/redmi_ax6000/hanwckf/diy-part1.sh
@@ -17,6 +17,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git kiddin9 https://github.com/kiddin9/kwrt-packages' >>feeds.conf.default
-#sed -i '$a src-git kenzok8 https://github.com/kenzok8/small-package' feeds.conf.default
+#echo 'src-git kiddin9 https://github.com/kiddin9/kwrt-packages' >>feeds.conf.default
+sed -i '$a src-git kenzok8 https://github.com/kenzok8/small-package' feeds.conf.default
 
