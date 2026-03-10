0258e3ce31fb504b3ba1567d3c62077c87e16c88
zjwxh <61620117+zjwxh@users.noreply.github.com>
2026-03-10 09:23:21 +0800

    Update diy-part1.sh

--- custom/redmi_ax6000/hanwckf/diy-part1.sh
+++ custom/redmi_ax6000/hanwckf/diy-part1.sh
@@ -11,14 +11,14 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i "/helloworld/d" "feeds.conf.default"
 #echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git kiddin9 https://github.com/kiddin9/kwrt-packages' >>feeds.conf.default
-sed -i '$a src-git kenzok8 https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '$a src-git kenzok8 https://github.com/kenzok8/small-package' feeds.conf.default
 # DIY part1 script for Redmi AX6000 (hanwckf branch)
 # 主要作用：修改feeds源、添加OpenClash官方源
 
