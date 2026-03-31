851acffcd3b88f61900b9152956e9d9ee2afc6aa
zjwxh <61620117+zjwxh@users.noreply.github.com>
2026-03-31 12:22:40 +0800

    Update diy-part1.sh

--- custom/redmi_ax6000/hanwckf/diy-part1.sh
+++ custom/redmi_ax6000/hanwckf/diy-part1.sh
@@ -11,6 +11,7 @@
 #
 
 # Uncomment a feed source
+src-git lienol https://github.com/Lienol/openwrt-package' >>feeds.conf.default
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i "/helloworld/d" "feeds.conf.default"
 #echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
