76c0d5cd1a1e2ac585f324cb579c72d9e77df384
zjwxh <61620117+zjwxh@users.noreply.github.com>
2026-05-14 12:28:11 +0800

    Update diy-part1.sh

--- custom/redmi_ax6/basic/diy-part1.sh
+++ custom/redmi_ax6/basic/diy-part1.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+# 1. 添加 kenzok8 常用包源
 sed -i '$a src-git kenzok8 https://github.com/kenzok8/small-package' feeds.conf.default
+
+# 2. 备份原始 feeds
+cp feeds.conf.default feeds.conf.default.bak
+
+# 3. 移除可能冲突的旧 OpenClash 源，强制添加最新版
+sed -i '/openclash/d' feeds.conf.default
+echo "src-git openclash https://github.com/vernesong/OpenClash.git" >> feeds.conf.default
+
+echo "DIY part1 completed: OpenClash official feed added!"
